8e2163d410163245ae6dbb0c429da2048f8b5ac9
CalmermanBenjaminNTI <benjamin@calmerman.se>
2023-09-06 15:22:29 +0200

    Drawable magic circles on screen

--- main.c
+++ main.c
@@ -1,23 +1,32 @@
 #include "raylib.h"
 #include "raymath.h"
 #include "stdbool.h"
+#include "stdlib.h"
 
-typedef enum {
+typedef struct Vector2 v2f;
+
+typedef enum
+{
     none,
     square,
     triangle,
     cicle,
     execute
-}magicCircleRing;
+} magicCircleRing;
 
 int main()
 {
-    Vector2 windowSize = {1000,1000};
-    InitWindow(windowSize.x, windowSize.y, "Spell Jam");
+    v2f windowMiddle = {500, 500};
+    InitWindow(windowMiddle.x * 2, windowMiddle.y * 2, "Spell Jam");
     bool aPressed = false;
-    bool bPressed = false;
-    bool cPressed = false;
+    bool wPressed = false;
+    bool dPressed = false;
     bool spacePressed = false;
+
+    magicCircleRing magicCircle[16];
+    int ringCount = 0;
+    float angle = 0;
+    float angleCoef = 1;
 
     while (!WindowShouldClose())
     {
@@ -25,14 +34,102 @@ int main()
         // INPUTS
         // ------
         aPressed = IsKeyPressed(KEY_A);
-        bPressed = IsKeyPressed(KEY_B);
-        cPressed = IsKeyPressed(KEY_C);
+        wPressed = IsKeyPressed(KEY_W);
+        dPressed = IsKeyPressed(KEY_D);
         spacePressed = IsKeyPressed(KEY_SPACE);
 
+        if (spacePressed)
+        {
+            ringCount = 0;
+        }
+        if (aPressed)
+        {
+            magicCircle[ringCount] = square;
+            ringCount++;
+        }
+        if (wPressed)
+        {
+            magicCircle[ringCount] = triangle;
+            ringCount++;
+        }
+        if (dPressed)
+        {
+            magicCircle[ringCount] = cicle;
+            ringCount++;
+        }
+
         BeginDrawing();
-        DrawCircleLines(250, 250, 50, PINK);
-        Vector2 pos = Vector2Scale(Vector2Normalize((Vector2){1, 1}), 50);
-        DrawRectangleLines(250 - pos.x, 250 - pos.y, 2 * pos.x, 2 * pos.y, PINK);
+        ClearBackground(BLACK);
+
+        // Draw magic circle
+        int radius = 10;
+        int squareCount = 0;
+        int triangleCount = 0;
+        angle = fabs(angle);
+        angle += 1 * GetFrameTime();
+        if (angle > 360)
+        {
+            angle -= 360;
+        }
+        for (int i = 0; i < ringCount; i++)
+        {
+            switch (magicCircle[i])
+            {
+            case square:
+            {
+                if (squareCount > 0)
+                {    
+                    DrawPolyLines(windowMiddle,4,radius,(-angle * angleCoef * radius) + 45,PINK);
+                    squareCount = 0;
+                    continue;
+                }
+                radius = Vector2Length((Vector2){radius, radius});
+                DrawPolyLines(
+                    windowMiddle,
+                    4,
+                    radius,
+                    angle * angleCoef * radius,
+                    PINK);
+                angle = -angle;
+                squareCount ++;
+                triangleCount = 0;
+            }
+            break;
+            case triangle:
+            {
+               if (triangleCount > 0)
+                {
+                    DrawPolyLines(windowMiddle,3,radius,(-angle * angleCoef * radius) + 60, PINK);
+                    triangleCount = 0;
+                    continue;
+                }
+                radius = radius / sin(30 * DEG2RAD);
+                DrawPolyLines(windowMiddle,3,radius,angle * angleCoef * radius, PINK);
+                squareCount = 0;
+                angle = -angle;
+                triangleCount ++;
+            }
+            break;
+            default:
+            {
+                if (i != 0)
+                {
+                    if (magicCircle[i-1] == cicle)
+                    {
+                        radius += 10;
+                    }
+                }
+                DrawCircleLines(
+                    windowMiddle.x,
+                    windowMiddle.y,
+                    radius, PINK);
+                    squareCount = 0;
+                    triangleCount = 0;
+            } break;
+            }
+        }
+        DrawText(TextFormat("%f",angle),20,20,18,WHITE);
+        
         EndDrawing();
     }
     return 0;
