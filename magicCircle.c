ab6d75c839166f880b29afcd5fb217fc42629341
CalmermanBenjaminNTI <benjamin@calmerman.se>
2023-09-11 15:29:44 +0200

    moonbeam animation

--- magicCircle.c
+++ magicCircle.c
@@ -3,7 +3,21 @@
 #include "stdbool.h"
 #include "stdlib.h"
 
+#define CircleRotation(radius, angle) ((angle * 3) / (radius * 0.0002 + 0.01))
+
 typedef struct Vector2 v2f;
+
+void DrawSpellMoonBeam(Vector2 targetLocation)
+{
+    int dave = GetRandomValue(200,255);
+    for (int i = 80; i > 20; i--)
+    {
+        DrawEllipse(targetLocation.x,targetLocation.y,i*2,i,(Color){255,255,255,10});
+    }
+    
+    DrawRectangle(targetLocation.x - 40, targetLocation.y-1000, 80, 1000, (Color){dave,dave,dave,255});
+    DrawEllipse(targetLocation.x,targetLocation.y,40,20,(Color){dave,dave,dave,255});
+}
 
 typedef enum
 {
@@ -14,15 +28,8 @@ typedef enum
     execute
 } magicCircleRing;
 
-float CircleRoation(int radius, float angle)
-{
-    return (angle * 3) / (radius * 0.0002 + 0.01);
-}
-
 void DrawMagicCircle(Vector2 playerPosition, magicCircleRing magicCircle[16], int ringCount, float *angle)
 {
-    float angleCoef = 5;
-
     int radius = 50;
     int squareCount = 0;
     int triangleCount = 0;
@@ -46,7 +53,7 @@ void DrawMagicCircle(Vector2 playerPosition, magicCircleRing magicCircle[16], in
         {
             if (squareCount > 0)
             {
-                DrawPolyLines(playerPosition, 4, radius, -CircleRoation(radius, *angle) + 45, ringColor);
+                DrawPolyLines(playerPosition, 4, radius, -CircleRotation(radius, *angle) + 45, ringColor);
                 squareCount = 0;
                 continue;
             }
@@ -58,7 +65,7 @@ void DrawMagicCircle(Vector2 playerPosition, magicCircleRing magicCircle[16], in
                 playerPosition,
                 4,
                 radius,
-                CircleRoation(radius, *angle),
+                CircleRotation(radius, *angle),
                 ringColor);
             *angle = -*angle;
             squareCount++;
@@ -71,7 +78,7 @@ void DrawMagicCircle(Vector2 playerPosition, magicCircleRing magicCircle[16], in
             squareCount = 0;
             if (triangleCount == 1)
             {
-                DrawPolyLines(playerPosition, 3, radius, -CircleRoation(radius, *angle) + 60, ringColor);
+                DrawPolyLines(playerPosition, 3, radius, -CircleRotation(radius, *angle) + 60, ringColor);
                 triangleCount = 0;
                 continue;
             }
@@ -81,7 +88,7 @@ void DrawMagicCircle(Vector2 playerPosition, magicCircleRing magicCircle[16], in
                 {
                     radius = radius / sin(30 * DEG2RAD);
                 }
-                DrawPolyLines(playerPosition, 3, radius, CircleRoation(radius, *angle), ringColor);
+                DrawPolyLines(playerPosition, 3, radius, CircleRotation(radius, *angle), ringColor);
                 *angle = -*angle;
                 triangleCount = 1;
             }
@@ -92,7 +99,7 @@ void DrawMagicCircle(Vector2 playerPosition, magicCircleRing magicCircle[16], in
                     radius -= 10;
                     for (int j = 0; j < 12; j++)
                     {
-                        DrawPolyLines(Vector2Add(playerPosition, Vector2Rotate((Vector2){0, radius}, (j * 30 - 15 - CircleRoation(radius, -*angle)) * DEG2RAD)), 3, 10, j * -30 - 45 + CircleRoation(radius, -*angle), ringColor);
+                        DrawPolyLines(Vector2Add(playerPosition, Vector2Rotate((Vector2){0, radius}, (j * 30 - 15 - CircleRotation(radius, -*angle)) * DEG2RAD)), 3, 10, j * -30 - 45 + CircleRotation(radius, -*angle), ringColor);
                     }
                     radius += 10;
                     triangleCount = 0;
@@ -102,7 +109,7 @@ void DrawMagicCircle(Vector2 playerPosition, magicCircleRing magicCircle[16], in
                     radius += 10;
                     for (int j = 0; j < 12; j++)
                     {
-                        DrawPolyLines(Vector2Add(playerPosition, Vector2Rotate((Vector2){0, radius}, (j * 30 - CircleRoation(radius, *angle)) * DEG2RAD)), 3, 10, j * -30 + CircleRoation(radius, *angle), ringColor);
+                        DrawPolyLines(Vector2Add(playerPosition, Vector2Rotate((Vector2){0, radius}, (j * 30 - CircleRotation(radius, *angle)) * DEG2RAD)), 3, 10, j * -30 + CircleRotation(radius, *angle), ringColor);
                     }
                     radius += 10;
                     *angle = -*angle;
