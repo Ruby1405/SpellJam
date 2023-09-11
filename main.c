e0cdf1b156c0cdce49e9a882647e7d276d154a16
0n10n7 <mikael.kokeritz@gmail.com>
2023-09-11 10:32:48 +0200

    terrain generation framework (and merging)

--- main.c
+++ main.c
@@ -1,21 +1,49 @@
 #include <stdio.h>
 #include "raylib.h"
 
-bool* DrunkardsWalk (bool*, int height, int width, int staggering){
+typedef struct door{
+    int xpos;
+    int ypos;
+    char direction;//N,E,S,W
+}door;
+typedef struct room{
+    door[4];
+
+}room;
+room* RoomCreator(){
+    room[20][20] roomGrid={0};
+    return roomGrid;
+}
+//En funktion för random terrain generation med "the drunkards walk" algoritmen
+bool* DrunkardsWalk (bool* mapIn, int height, int width, int staggering){
     int drunkardsXPOS = width/2;
     
-    int DrunkardsYPOS = height/2;
+    int drunkardsYPOS = height/2;
+
+    bool drunkardOutOfBounds = false;
+    bool map[height][height]=mapIn;
 
     int i = 0;
-    while(((drunkardsXPOS <= width && drunkardsXPOS >= width)&&(DrunkardsYPOS <= height && DrunkardsYPOS >= height))||staggering<=i){
+    while (staggering&& drunkardOutOfBounds)
+    {
         i++;
+        switch (expression)
+        {
+        case drunkardsXPOS<0:
+            /* code */
+            break;
+        
+        default:
+            break;
+        }
     }
+    
 }
 
 int main(void)
 {   
     //1120,1280 är *5 
-    InitWindow(1120,1286 , "The Shoe Salesman"); //Ration ska vara 224, 256. Höjden behöver ökas med 8 för window rounding
+    InitWindow(1120,1286 , "SpellJam"); //Ration ska vara 224, 256. Höjden behöver ökas med 6 för window rounding på mac
 
     Rectangle rec = {0,0,1120,1280};
     while (!WindowShouldClose())
